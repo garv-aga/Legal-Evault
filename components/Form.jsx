b6265e0501695452bcd442e182ef6ae91181680b
garv-aga <garvagarwal2003@gmail.com>
2023-06-23 23:30:39 +0530

    improved styling

--- components/Form.jsx
+++ components/Form.jsx
@@ -32,15 +32,15 @@ const Form = () => {
   return (
     <form onSubmit={handleSubmit} className='mx-10'>
       <div className='bg-blue-gradient w-full h-full rounded-md p-0.5 mt-10 relative'>
-        <div className='absolute -top-4 -left-10 w-72 h-72 bg-[#33bbcf] rounded-full animate-blob1 filter blur-2xl opacity-20 animation-delay-4' />
-        <div className='absolute top-10 -right-20 w-72 h-72 bg-[#9dedf0] rounded-full animate-blob2 filter blur-2xl opacity-20' />
-        <div className='absolute -bottom-8 left-10 w-72 h-72 bg-[#def9fa] rounded-full animate-blob3 filter blur-2xl opacity-20 animation-delay-2' />
+        <div className='absolute -top-4 -left-10 w-72 h-72 bg-[#33bbcf] rounded-full animate-blob1 filter blur-2xl opacity-10 animation-delay-4' />
+        <div className='absolute top-10 -right-20 w-72 h-72 bg-[#9dedf0] rounded-full animate-blob2 filter blur-2xl opacity-10' />
+        <div className='absolute -bottom-8 left-10 w-72 h-72 bg-[#def9fa] rounded-full animate-blob3 filter blur-2xl opacity-10 animation-delay-2' />
 
         <div className='bg-black rounded-md text-white grid grid-rows-7 place-items-center p-[30px]' >
           <label for='file' className='font-poppins text-[28px]'>Image, Video or Audio: </label>
           <p className='font-poppins text-[12px]'>File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG, GLB, GLTF</p>
           <div className='flex space-y-4'>
-            <input type="file" id="file" accept=".jpg, .png, .gif, .svg, .mp4, .webm, .mp3, .wav, .ogg, .glb, .gltf" class="hidden" />
+            <input type="file" required id="file" accept=".jpg, .png, .gif, .svg, .mp4, .webm, .mp3, .wav, .ogg, .glb, .gltf" class="hidden" />
             <label for="file" class="cursor-pointer bg-black hover:bg-gray-900 text-white font-medium py-[100px] px-[150px] rounded-md border-2 border-dotted border-gray-400">
               <img src='/upload.svg' alt='upload' className='h-20 w-20 ' />
             </label>
@@ -49,13 +49,13 @@ const Form = () => {
             <label htmlFor="name" className='font-poppins text-[28px]'>Name:</label>
           </div>
           <div>
-            <input type="text" id="name" value={name} placeholder='Name your NFT' onChange={handleNameChange} className='bg-gray-800 text-gray-300 placeholder-gray-500 border-gray-600 focus:ring-gray-400 focus:border-gray-400 rounded-md py-2 px-4 font-poppins' />
+            <input type="text" id="name" value={name} placeholder='Name your NFT' onChange={handleNameChange} required className='bg-gray-800 text-gray-300 placeholder-gray-500 border-gray-600 focus:ring-gray-400 focus:border-gray-400 rounded-md py-2 px-4 font-poppins w-[30vw]' />
           </div>
           <div className='flex mt-10 mb-2'>
             <label htmlFor="description" className='font-poppins text-[28px]'>Description:</label>
           </div>
           <div>
-            <textarea id="description" value={description} onChange={handleDescriptionChange} className='bg-gray-800 text-gray-300 placeholder-gray-500 border-gray-600 focus:ring-gray-400 focus:border-gray-400 rounded-md py-2 px-4 font-poppins' placeholder='Provide a detailed description of your NFT.' />
+            <textarea id="description" value={description} onChange={handleDescriptionChange} className='bg-gray-800 text-gray-300 placeholder-gray-500 border-gray-600 focus:ring-gray-400 focus:border-gray-400 rounded-md py-2 px-4 font-poppins w-[30vw]' placeholder='Provide a detailed description of your NFT.' />
           </div>
           <button type="submit" className='flex mt-10 font-poppins text-[23px] bg-blue-gradient px-4 py-2 rounded-md text-black transform transition-all duration-300 hover:scale-110'>Mint NFT</button>
         </div>
